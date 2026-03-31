41dddb461d5b8144fc2f57053be050a2586dfa07
hyarlei <hyarleysf@gmail.com>
2026-03-31 17:07:28 -0300

    clean: remove duplicate files from root and fix folder structure

--- src/repositories/tasks-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-export interface TasksRepository {
-  create(data: Prisma.TaskCreateInput): Promise<any>;
-  findAll(): Promise<any[]>;
-  update(id: string | number, data: Prisma.TaskUpdateInput): Promise<any>;
-  delete(id: string | number): Promise<void>;
-}
