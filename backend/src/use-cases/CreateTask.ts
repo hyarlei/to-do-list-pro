3430854962a4c528daaba7e649f20ce5b90f9ae1
Hyarlei Silva <hyarleysf@gmail.com>
2026-04-02 15:29:55 -0300

    Reforming CRUD using SOLID principles

---
+++ backend/src/use-cases/CreateTask.ts
@@ -0,0 +1,34 @@
+import { TasksRepository } from '../repositories/tasks-repository';
+
+interface CreateTaskUseCaseRequest {
+    title: string;
+    description?: string;
+    completed?: boolean;
+    categoryId: number;
+}
+
+interface CreateTaskUseCaseResponse {
+    task: any;
+}
+
+export class CreateTaskUseCase {
+    constructor(private taskRepository: TasksRepository) {}
+
+    async execute({
+        title,
+        description,
+        completed = false,
+        categoryId,
+    }: CreateTaskUseCaseRequest): Promise<CreateTaskUseCaseResponse> {
+        const task = await this.taskRepository.create({
+            title,
+            description: description || undefined,
+            completed,
+            category: { connect: { id: categoryId } },
+        });
+
+        return {
+            task,
+        };
+    }
+}
